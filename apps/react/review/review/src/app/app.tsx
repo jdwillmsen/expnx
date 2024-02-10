0442af2bfd6ead3f93b8bb40f809f222370bf155
Jake Willmsen <jdwillmsen@gmail.com>
2024-02-09 20:37:32 -0600

    Add react review app

---
+++ apps/react/review/review/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="review" />
+    </div>
+  );
+}
+
+export default App;
