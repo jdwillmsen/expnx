ca3335177c4b8c84dd46da5a7e7130e2efb845bd
Jake Willmsen <jdwillmsen@gmail.com>
2024-02-10 20:28:05 -0600

    feat: Add provideEffects to e-commerce app config

--- apps/angular/e-commerce/e-commerce/src/app/app.config.ts
+++ apps/angular/e-commerce/e-commerce/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideRouter } from '@angular/router';
 import { provideState } from '@ngrx/store';
 import { appRoutes } from './app.routes';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
     provideAnimationsAsync(),
     provideState('', {}),
+    provideEffects([]),
   ],
 };
