8d8a4581feb8bdbb6379cb810077e3c097b7c2c6
Jake Willmsen <jdwillmsen@gmail.com>
2024-02-18 13:22:07 -0600

    chore: Clean up sb-app-1

--- apps/spring-boot/sb-app-1/build.gradle.kts
+++ apps/spring-boot/sb-app-1/build.gradle.kts
@@ -1,7 +1,10 @@
+import com.google.cloud.tools.jib.api.buildplan.ImageFormat
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("com.google.cloud.tools.jib") version "3.4.0"
 	id("maven-publish")
 }
 
@@ -10,6 +13,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_21
+	targetCompatibility = JavaVersion.VERSION_21
 }
 
 configurations {
@@ -23,29 +27,46 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.kafka:spring-kafka")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("io.micrometer:micrometer-registry-prometheus")
-	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	testImplementation("org.springframework.kafka:spring-kafka-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:kafka")
-	testImplementation("org.testcontainers:postgresql")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
 springBoot {
 	buildInfo()
 }
+
+jib {
+	from {
+		image = "azul/zulu-openjdk-alpine:21-jre"
+		auth {
+			username = property("dockerUsername").toString()
+			password = property("dockerPassword").toString()
+		}
+	}
+	to {
+		image = "jdwillmsen/sb-app-1"
+		auth {
+			username = property("dockerUsername").toString()
+			password = property("dockerPassword").toString()
+		}
+		tags = setOf("latest", "${project.version}")
+	}
+	container {
+		format = ImageFormat.OCI
+	}
+}
+
 publishing {
 	publications {
 		create<MavenPublication>("mavenJava") {
---
+++ apps/spring-boot/sb-app-1/src/main/java/com/expnx/main/controllers/SystemController.java
@@ -0,0 +1,13 @@
+package com.expnx.main.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SystemController {
+
+    @GetMapping("/version")
+    public String getVersion() {
+        return "0.0.1";
+    }
+}
