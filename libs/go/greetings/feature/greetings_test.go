4a01bbdbcb53fd2685d24a16a8f896c86d587798
Jake Willmsen <jdwillmsen@gmail.com>
2024-03-02 17:14:25 -0600

    test: Add additional unit tests for greetings feature library

--- libs/go/greetings/feature/greetings_test.go
+++ libs/go/greetings/feature/greetings_test.go
@@ -1,12 +1,27 @@
 package greetings
 
 import (
+	"regexp"
 	"testing"
 )
 
-func TestGreeting(t *testing.T) {
-	_, err := Greeting("works")
-	if err != nil {
-		t.Error(err)
+// TestGreetingName calls greetings.Greeting with a name, checking
+// for a valid return value.
+func TestGreetingName(t *testing.T) {
+	name := "Jake"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Greeting(name)
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Greeting(%q) = %q, %v, want match for %#q, nil`, name, msg, err, want)
+	}
+}
+
+// TestGreetingEmpty calls greetings.Greeting with an empty string,
+// checking for an error.
+func TestGreetingEmpty(t *testing.T) {
+	name := ""
+	msg, err := Greeting(name)
+	if msg != "" || err == nil {
+		t.Fatalf(`Greeting(%q) = %q, %v, want "", error`, name, msg, err)
 	}
 }
